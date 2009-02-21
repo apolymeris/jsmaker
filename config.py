6cfe76da27a80186115cf0ee60fa076a0933af21
mlowicki <mlowicki@136927e6-ffaa-11dd-99c0-75d572ba1ddd>
2009-02-21 22:54:04 +0000

    basic structure

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+﻿#!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
 #
@@ -10,22 +10,18 @@ CONFIG = {
         "version"    : "0.1",
         "authors"   : "Michał Łowicki"
     },
-    "FILES" : [
+    "MODULES" : [
         {
-            "output"   : "output#1.js",
+            "output"   : "build/output#1.js",
             "dirs"       : [
-                "dir#1",
-                "dir#2",
-                "dir#3"
+                "js/js#1",
+                "js/js#2"
             ]
         },
         {
-            "output"   : "output#2.js",
+            "output"   : "build/output#2.js",
             "dirs"       : [
-                "dir#4",
-                "dir#5",
-                "dir#6",
-                "dir#7"
+                "js/js#3"
             ]
         }
     ],
