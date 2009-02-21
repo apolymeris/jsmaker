1083d782cd83bfa15a8d67e9dc4ce087119917ae
mlowicki <mlowicki@136927e6-ffaa-11dd-99c0-75d572ba1ddd>
2009-02-21 22:54:04 +0000

    basic structure

--- trunk/make.py
+++ trunk/make.py
@@ -5,12 +5,70 @@
 Usage: make.py [ -h|--help ] [ -d|--debug ] [ -r|--release ]
 """
 
-import getopt, sys
+import getopt, sys, os
 from config import CONFIG
 
+
+class Module:
+	"""
+	Basic Module
+	"""
+	def __init__( self, obj ):
+		self.output = obj["output"]
+		self.dirs = obj["dirs"]
+		self.run()
+	
+	def run( self ):
+		raise NotImplemented
+
+
+class CSS_Module( Module ):
+	"""
+	CSS Module
+	"""
+	def __init__( self ):
+		raise NotImplemented
+
+
+class JS_Module( Module ):
+	"""
+	JavaScript Module
+	"""
+	def __init__( self, obj ):
+		self.output = obj["output"]
+		self.dirs = obj["dirs"]
+		self.run() 
+		
+	def run( self ):
+		""" start builder """
+		output = open( self.output, "w" )
+		output.write( self.get_content() )
+		output.close()
+		print self.output + " created successfully."
+
+	def get_content( self ):
+		""" return module content """
+		content = ""
+		for dir in self.dirs:
+			content += self.get_dir_content( dir )
+		return content
+		
+	def get_dir_content( self, dir ):
+		""" return dir content """
+		files = os.listdir( dir )
+		content = ""
+		for file in files:
+			if os.path.isdir( dir + "/" + file ): # recursion
+				content += self.get_dir_content( dir + "/" + file )
+			else:
+				f = open( dir + "/" + file, "r" )
+				content += "\n" + f.read() + "\n"
+				f.close()
+		return content 
+
 def compress( obj ):
-    print CONFIG
-    print obj
+    for module in CONFIG[ "MODULES" ]:
+		JS_Module( module )
 
 def main():
     try:
@@ -24,7 +82,8 @@ def main():
         "release"       : False,
         "debug"         : False,
         "modules"    : []
-    }   
+    }  
+	
     for o, a in opts:
         if o in ( "-r", "--release" ):
             cfg[ "release" ] = True
